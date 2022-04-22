3028ba6ac0cb45d76f0354eaeef6c4dea2bf2d16
Heukkell <falcfmpcv@gmail.com>
2022-04-22 14:16:55 +0900

    Implement Nav Component CSS
    - Nav 컴포넌트 관련 CSS 및 레이아웃, 아이콘 작업 완료

--- client/src/Components/NAV.js
+++ client/src/Components/NAV.js
@@ -2,54 +2,76 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
+import NavIcon from './NAVIcon';
+
+
+// 로고영역
+
+
+
+
+// 네비게이션바 영역을 정하는 컴포넌트
 const NavBar = styled.div`
-    /* position:fixed ;
-    top: 0; */
+    position:fixed ;
+    top: 0;
     display: flex;
     flex-wrap:nowrap;
     flex-direction: row;
-    background-color: white;
+    justify-content: space-between;
+    
+    background-color: rgb(255,255,255,0.7);
     width: 100%;
     height: 100px;
     box-shadow: 0px 0px 12px rgb(225,208,205,0.5);
+    z-index: 5;
 `
 
-const NavIconBox = styled.div`
-    width: 80px;
-    height: 80px;
+// Logo를 가진 아이콘
+const LogoIconBox = styled.div`
     display: flex;
-    justify-content: center;
-    align-items: center;
-    color: black;
-    background-color:white ;
+    width:120px; height: 80px;
+    
     margin: 10px;
+    user-select: none;
+`
+// LogoImage
+const LogoImg = styled.img.attrs({
+    src:"/Resource/ICO/ICO_Logo.png"
+    
+})`
+
+    &:hover{
+        filter:drop-shadow(0px 0px 5px black);
+    }
 `
 
-const NavIcon = ({iconName})=>{
 
-    return(
-        <NavIconBox>
-            <span>{iconName}</span>
-        </NavIconBox>
-    )
-}
+// NAVIcon 을 포함하는 유저 메뉴
+const NavIconBox = styled.div`
+    
+    display:flex ;
+    align-self: flex-end ;
+`
 
 const NAV = ()=>{
     let isAccountValid = false;
 
+
     return (
-        <>
-            <NavBar>
-                <Link to="/"><NavIcon iconName={"view"}/></Link>
-                <Link to="/writing"><NavIcon iconName={"writing"}/></Link>
-                <Link to="/store"><NavIcon iconName={"store"}/></Link>
-                {
-                    isAccountValid? // 계정 로그인이 되어있는가? 마이페이지 : 로그인페이지
-                    (<Link to="/account"><NavIcon iconName={"account"}/></Link>):
-                    (<Link to="/login"><NavIcon iconName={"login"}/></Link>)
-                }
-            </NavBar>
-        </>
+    
+        <NavBar>
+            <Link to="/" ><LogoIconBox><LogoImg/></LogoIconBox></Link>
+
+            <NavIconBox>
+                <NavIcon url="/" iconName="view" />
+                <NavIcon url="/writing" iconName="writing" />
+                <NavIcon url="/store" iconName="store" />
+                { isAccountValid? //계정 로그인이 되어있는가? 마이페이지 : 로그인페이지
+                <NavIcon url="/account" iconName="account" /> : 
+                <NavIcon url="/login" iconName="login" />}
+            </NavIconBox>
+        </NavBar>
+    
     )
 }
 
---
+++ client/src/Components/NAVIcon.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
+
+const NavIconBox = styled.div`
+    width: 80px;
+    height: 80px;
+    display: flex;
+
+    flex-direction: column;
+
+    justify-content: center;
+    align-items: center;
+    color: black;
+    /* background-color:gray ; */
+    margin: 10px;
+    user-select:none;
+`
+const NavIconImg = styled.img`
+    width: 50px;
+    height: 50px;
+
+    &:hover{
+        filter:drop-shadow(0 0 5px black)
+    }
+`
+
+const NavIconName = styled.span`
+    font-weight: 600;
+`
+
+
+
+
+const NavIcon = ({url,iconName})=>{
+
+    let iconSrc = "/Resource/ICO/"
+    switch(iconName){
+        case "view": {iconSrc+="ICO_View.png"; break}
+        case "writing" : {iconSrc+="ICO_Writing.png"; break}
+        case "store" : {iconSrc+="ICO_Store.png"; break}
+        case "login" : {iconSrc+="ICO_Login.png"; break}
+        case "account" : {iconSrc+="ICO_Account.png";break}
+        default : {iconSrc+="ICO_IconDumy5050.png";break}
+    }
+    console.log(iconSrc);
+
+    return(
+        <Link to={url} style={{textDecoration:"none"}}>
+            <NavIconBox>
+                <NavIconImg src={iconSrc}></NavIconImg>
+                <NavIconName>{iconName ||"페이지명"}</NavIconName>
+            </NavIconBox>
+        </Link>
+    )
+}
+
+export default NavIcon;
