190c91b8a9a18f494724c5252ab63e48ee1cc75b
Robin Hanjun Kwon <khj02021@gmail.com>
2022-04-25 15:29:23 +0900

    Complete test on Rinkeby and Refactor codes

--- server/web3.js
+++ server/web3.js
@@ -1,8 +1,8 @@
 import Web3 from "web3";
+import dotenv from 'dotenv';
 
-export const web3 = new Web3(
+dotenv.config();
+
+export const web3 = new Web3(process.env.NODE_URI || "http://127.0.0.1:7545");
   // new Web3.providers.HttpProvider(
   //   "https://rinkeby.infura.io/v3/0de3b94811a7482594d0a137397d3755"
-  // )
-  new Web3.providers.HttpProvider("http://127.0.0.1:7545")
-);
