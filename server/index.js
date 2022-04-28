0a766360cd1f54a9c35f3e9e87f2aa029b4e9a77
Heukkell <falcfmpcv@gmail.com>
2022-04-28 11:59:12 +0900

    Change DB name

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017";
 const PORT = process.env.PORT || 4000;
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(MONGODB_URI+"SLS")
   .then(() =>
     app.listen(PORT, async () => {
       console.log(`Server is running on port ${PORT}...`);
